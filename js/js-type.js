c0c9989bb414227fca1c7f0e20ef2ff9878cafa1
JasonWoo <794054202@qq.com>
2016-12-27 02:02:48 +0800

    typeof instanceof

---
+++ js/js-type.js
@@ -0,0 +1,10 @@
+// from http://www.nowamagic.net/librarys/veda/detail/1250
+
+// instanceof
+
+// typeof
+
+// 判断数组
+var array = [];
+typeof array === 'object'  // true
+array instanceof Array // true
